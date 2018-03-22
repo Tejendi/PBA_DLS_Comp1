6c98479dc41bb3367c3a378461b7c9c0d91c2ea2
Daniel Bloch Christensen <daniel_b_christensen@hotmail.com>
2018-03-22 21:25:26 +0100

    Updated code and added monitor data

--- ConsumerWithMessageExpiration/Consumer.cs
+++ ConsumerWithMessageExpiration/Consumer.cs
@@ -3,6 +3,7 @@ using RabbitMQ.Client.Events;
 using System;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
 
 namespace Consumer
 {
@@ -10,19 +11,28 @@ namespace Consumer
     {
         static void Main(string[] args)
         {
+
+            var consumerInstance = Guid.NewGuid();
+
+            var logPath = "C:\\temp\\" ;
+
+            if (!Directory.Exists(logPath))
+                Directory.CreateDirectory(logPath);
+
+            var logFile = File.Create(logPath + consumerInstance + ".txt");
+
             var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var logWriter = new StreamWriter(logFile))
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "task_queue",
-                                     durable: true,
-                                     exclusive: false,
-                                     autoDelete: false,
-                                     arguments: null);
+                channel.QueueDeclare(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
 
-                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 20, global: false);
 
-                Console.WriteLine(" [*] Waiting for messages.");
+                Console.WriteLine($"{consumerInstance}: Waiting for messages.");
+                logWriter.WriteLine($"{consumerInstance}: Log file created at: {logPath}");
+                logWriter.WriteLine($"{consumerInstance}: Waiting for messages.");
 
                 var consumer = new EventingBasicConsumer(channel);
                 consumer.Received += (model, ea) =>
@@ -30,21 +40,22 @@ namespace Consumer
                     var body = ea.Body;
                     var message = ByteArrayToObject(body);
 
-                    //Console.WriteLine(" [x] Received {0}", message);
-                    Console.WriteLine($"Recieved a message: {message} at: {DateTime.Now}");
+                    Console.WriteLine($"{consumerInstance}: Received a message: {message} at: {DateTime.Now}");
+                    logWriter.WriteLine($"{consumerInstance}: Received a message: {message} at: {DateTime.Now}");
+
                     ValueTuple<string, string> values = (ValueTuple<string, string>)message;
 
                     var primes = PrimeCheckerService.CountPrimes(long.Parse(values.Item1), long.Parse(values.Item2));
 
-                    Console.WriteLine($"Result from {message}: {primes}, finished at {DateTime.Now}");
-
-
-                    //Console.WriteLine(" [x] Done");
+                    Console.WriteLine($"{consumerInstance}: Result from {message}: {primes}, finished at {DateTime.Now}");
+                    logWriter.WriteLine($"{consumerInstance}: Result from {message}: {primes}, finished at {DateTime.Now}");
 
                     channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
                 };
-                channel.BasicConsume(queue: "task_queue",
-                                     consumer: consumer);
+
+                channel.BasicConsume(queue: "task_queue", consumer: consumer);
+
+
 
                 Console.WriteLine(" Press [enter] to exit.");
                 Console.ReadLine();
--- PublisherWithMessageExpiration/Publisher.cs
+++ PublisherWithMessageExpiration/Publisher.cs
@@ -1,5 +1,6 @@
 ﻿using RabbitMQ.Client;
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
@@ -13,36 +14,38 @@ namespace Publisher
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "task_queue",
-                                     durable: true,
-                                     exclusive: false,
-                                     autoDelete: false,
-                                     arguments: null);
+                channel.QueueDeclare(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
 
-                
-                
                 var properties = channel.CreateBasicProperties();
                 properties.Persistent = true;
 
-                for (long i = 10000000; i < 1000000000; i += 5000000)
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                while (stopwatch.Elapsed < TimeSpan.FromMinutes(10))
                 {
-                    for (int t = 0; t < 10; t++)
-                    
+                    for (long i = 1; i < 10000000; i += 200000)
                     {
-                        Console.WriteLine($"Creating request at: {DateTime.Now} for Count Primes: 1, 10");
+                        for (int t = 0; t < 10; t++)
+
+                        {
+                            Console.WriteLine($"Creating request at: {DateTime.Now} for Count Primes: 1, 10");
+                            channel.BasicPublish(exchange: "",
+                                             routingKey: "task_queue",
+                                             basicProperties: properties,
+                                             body: GetMessage(("1", "10")));
+                        }
+
+                        Console.WriteLine($"Creating request at: {DateTime.Now} for Count Primes: 1, {i}");
                         channel.BasicPublish(exchange: "",
                                          routingKey: "task_queue",
                                          basicProperties: properties,
-                                         body: GetMessage(("1", "10")));
+                                         body: GetMessage(("1", i.ToString())));
+
                     }
-
-                    Console.WriteLine($"Creating request at: {DateTime.Now} for Count Primes: 1, {i}");
-                    channel.BasicPublish(exchange: "",
-                                     routingKey: "task_queue",
-                                     basicProperties: properties,
-                                     body: GetMessage(("1",i.ToString())));
-
                 }
+
+                stopwatch.Stop();
 
                 Console.WriteLine(" [x] Sent");
             }
